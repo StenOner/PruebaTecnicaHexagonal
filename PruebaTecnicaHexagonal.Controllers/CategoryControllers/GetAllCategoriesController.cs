087c2a40f0a5cd44cbd6070f021f9a878e91b8ae
Leonardo Acevedo <stenoner90@gmail.com>
2024-12-08 22:35:44 -0500

    added controllers for category

---
+++ PruebaTecnicaHexagonal.Controllers/CategoryControllers/GetAllCategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaHexagonal.DTOs.CategoryDTOs;
+using PruebaTecnicaHexagonal.Presenters;
+using PruebaTecnicaHexagonal.UseCasesPorts.CategoryUseCasesPorts.GetAllCategories;
+
+namespace PruebaTecnicaHexagonal.Controllers.CategoryControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllCategoriesController
+    {
+        readonly IGetAllCategoriesInputPort _inputPort;
+        readonly IGetAllCategoriesOutputPort _outputPort;
+
+        public GetAllCategoriesController(IGetAllCategoriesInputPort inputPort, IGetAllCategoriesOutputPort outputPort)
+            => (_inputPort, _outputPort) = (inputPort, outputPort);
+
+        [HttpGet]
+        public async Task<IEnumerable<CategoryDTO>> GetAll()
+        {
+            await _inputPort.Handle();
+            return ((IPresenter<IEnumerable<CategoryDTO>>)_outputPort).Content;
+        }
+    }
+}
---
+++ PruebaTecnicaHexagonal.Controllers/CategoryControllers/GetCategoryByIdController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaHexagonal.DTOs.CategoryDTOs;
+using PruebaTecnicaHexagonal.Presenters;
+using PruebaTecnicaHexagonal.UseCasesPorts.CategoryUseCasesPorts.GetCategoryById;
+
+namespace PruebaTecnicaHexagonal.Controllers.CategoryControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetCategoryByIdController
+    {
+        readonly IGetCategoryByIdInputPort _inputPort;
+        readonly IGetCategoryByIdOutputPort _outputPort;
+
+        public GetCategoryByIdController(IGetCategoryByIdInputPort inputPort, IGetCategoryByIdOutputPort outputPort)
+            => (_inputPort, _outputPort) = (inputPort, outputPort);
+
+        [HttpGet]
+        public async Task<CategoryDTO> Get(Guid id)
+        {
+            await _inputPort.Handle(id);
+            return ((IPresenter<CategoryDTO>)_outputPort).Content;
+        }
+    }
+}
